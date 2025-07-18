f10244363be750a4e351b62e6bae99689f77f99e
Tatiana <tatisalexa16@gmail.com>
2025-07-18 12:29:52 -0500

    administrador de Django y CRUD

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -8,3 +8,6 @@ class Member(models.Model):
     gender = models.CharField(max_length=255, null=True)
     birth_date = models.DateField(null=True)
     email = models.EmailField(max_length=255, null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
